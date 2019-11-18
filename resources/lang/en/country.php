6182dfcff0bf5ba66323974c639bc8cd42de89c9
Jonathan SARDO <sardoj@gmail.com>
2019-11-18 15:53:05 -0300

    feat(lang): Adds en and fr translations

---
+++ resources/lang/en/country.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'country' => 'Countries',
+    'country.single' => 'Country',
+    'block.general' => 'General information',
+    'field.name' => 'Name',
+    'field.code' => 'Code',
+    'field.iso_alpha3' => 'Iso alpha 3',
+    'field.currency_code' => 'Currency',
+    'field.fips_code' => 'Fips code',
+    'field.iso_numeric' => 'Iso numeric',
+    'block.location' => 'Location',
+    'field.north' => 'North',
+    'field.south' => 'South',
+    'field.east' => 'East',
+    'field.west' => 'West',
+    'block.other' => 'Other information',
+    'field.capital' => 'Capital',
+    'field.continent_name' => 'Continent',
+    'field.continent_code' => 'Continent code',
+    'field.languages' => 'Languages',
+    'field.geoname_id' => 'Geoname id',
+];
---
+++ resources/lang/fr/country.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'country' => 'Pays',
+    'country.single' => 'Pays',
+    'block.general' => 'Informations générales',
+    'field.name' => 'Nom',
+    'field.code' => 'Code',
+    'field.iso_alpha3' => 'Iso alpha 3',
+    'field.currency_code' => 'Devise',
+    'field.fips_code' => 'Code fips',
+    'field.iso_numeric' => 'Iso numeric',
+    'block.location' => 'Coordonnées',
+    'field.north' => 'Nord',
+    'field.south' => 'Sud',
+    'field.east' => 'Est',
+    'field.west' => 'Ouest',
+    'block.other' => 'Informations complémentaires',
+    'field.capital' => 'Capitale',
+    'field.continent_name' => 'Continent',
+    'field.continent_code' => 'Code continent',
+    'field.languages' => 'Langues',
+    'field.geoname_id' => 'Geoname id',
+];
